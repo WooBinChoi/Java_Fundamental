64f39db78bba49dd3c2bc3b56ddda318e1207372
WooBinChoi <eternal921@naver.com>
2021-05-11 17:47:21 +0900

    2021-05-11 v1

--- src/java_20210511/ChildDemo.java
+++ src/java_20210511/ChildDemo.java
@@ -12,7 +12,12 @@ public class ChildDemo {
 		System.out.println(child.height);
 		
 		Child child2 = new Child("최우빈", "서울", 30, "010-9090-8765", 50.3, 155.2);
-		
+		System.out.println(child.name);
+		System.out.println(child.address);
+		System.out.println(child.age);
+		System.out.println(child.phone);
+		System.out.println(child.weight);
+		System.out.println(child.height);
 	}
 		
 }
