474618105c60ca3bb14233210f4f54c5e439af90
WooBinChoi <eternal921@naver.com>
2021-05-12 17:04:15 +0900

    2021-05-12 v1

---
+++ src/java_20210512/StringDemo.java
@@ -0,0 +1,29 @@
+package java_20210512;
+
+public class StringDemo {
+	public static void m(int a, int[] b, String s) {
+		a = 20;
+		b[3]=40000;
+		s += "test";
+	}
+	public static void main(String[] args) {
+		int a = 10;
+		int[] b = {1,2,3,4};
+		String s = "abc";
+		
+		System.out.println(a+","+b[3]+","+s);
+		//CallByReference에서 유일하게 안바뀌는 건 String
+		m(a,b,s);
+		System.out.println(a+","+b[3]+","+s);
+		String str1 = new String("hello");
+		String str2 = new String("hello");
+		String str3 = "test";
+		String str4 = "test";
+		
+		System.out.println(str1==str2);
+		System.out.println(str1.equals(str2));
+		
+		System.out.println(str3==str4);
+		System.out.println(str3.equals(str4));
+	}
+}
---
+++ src/java_20210512/WrapperDemo.java
@@ -0,0 +1,23 @@
+package java_20210512;
+
+import java.util.ArrayList;
+
+public class WrapperDemo {
+	public static void main(String[] args) {
+		//ctrl + shift + o => auto import
+		//자바 1.4 이전 버전에서는 primitive data type를 collection에
+		//저장할 수 없었기 때문에 primitive data type을 객체화 할 수 있는
+		//Wrapper Class으로 변환하여 저장해야 했다.
+		//아래와 같이 1을 저장할 수 없음.
+		ArrayList list = new ArrayList();
+		list.add(new Integer(1));
+		
+		//1. primitive data type => wrapper class(valueOf(~))
+		Integer i1 = new Integer.valueOf(10);
+		Integer i2 = new Integer.valueOf("20");
+		
+		//wrapper class => primitive data type( intValue() )
+		int i3 = i1.intValue();
+		int i4 = i2.intValue();
+	}
+}
