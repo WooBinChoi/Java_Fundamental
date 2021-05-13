d70ce5d0f3318bea957ca1c98a2eef780d2b1f91
WooBinChoi <eternal921@naver.com>
2021-05-13 15:38:56 +0900

    2021-05-13 v1

--- src/java_20210512/StringDemo.java
+++ src/java_20210512/StringDemo.java
@@ -1,16 +1,23 @@
 package java_20210512;
 
 public class StringDemo {
+	public static String reverse(String str) {
+		return "";
+	}
+	
 	public static void m(int a, int[] b, String s) {
 		a = 20;
 		b[3]=40000;
-		s += "test";
+		s += "123";
 	}
 	public static void main(String[] args) {
+		String str = "beautiful"; // => "lufituaeb";
+		System.out.println(str.substring(1,2));
+		String str5 = reverse(str);
+
 		int a = 10;
 		int[] b = {1,2,3,4};
 		String s = "abc";
-		
 		System.out.println(a+","+b[3]+","+s);
 		//CallByReference에서 유일하게 안바뀌는 건 String
 		m(a,b,s);
